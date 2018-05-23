9ce8a30d0f61bfc1077cadde62aa958375f7cf13
Tom Davis <tom.davis2@tesco.com>
2018-05-23 23:32:22 +0100

    Create web docker image and kubernetes deployment.

--- publish-images.ps1
+++ publish-images.ps1
@@ -1,6 +1,8 @@
 
 docker tag publisher akstutorialacr.azurecr.io/publisher
 docker tag consumer akstutorialacr.azurecr.io/consumer
+docker tag web akstutorialacr.azurecr.io/web
 
 docker push akstutorialacr.azurecr.io/publisher
 docker push akstutorialacr.azurecr.io/consumer
+docker push akstutorialacr.azurecr.io/web
