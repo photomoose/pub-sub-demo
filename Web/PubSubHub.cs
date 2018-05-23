66b15ad43b57aa59b8fdb9e07a36895e8f65e352
Tom Davis <tom.davis2@tesco.com>
2018-05-23 22:52:39 +0100

    Add Web project.

---
+++ Web/PubSubHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Web
+{
+	public class PubSubHub : Hub
+	{
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+	}
+}
