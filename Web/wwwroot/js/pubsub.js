6cf139b9ed605f88ccdd4787245db363c42fdd16
Tom Davis <tom.davis2@tesco.com>
2018-05-24 09:40:02 +0100

    Remove textboxes from web.

--- Web/wwwroot/js/pubsub.js
+++ Web/wwwroot/js/pubsub.js
@@ -19,10 +19,3 @@ connection.on("ReceiveMessage", (user, message) => {
 });
 
 connection.start().catch(err => console.error(err.toString()));
-
-document.getElementById("sendButton").addEventListener("click", event => {
-    const user = document.getElementById("userInput").value;
-    const message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
-    event.preventDefault();
-});
